80cd33b1b752191fd274f10131cd59042c80625f
ikeda042 <ikeda042main@gmail.com>
2023-11-25 15:06:30 +0900

    Demo scripts

---
+++ demo.py
@@ -0,0 +1,4 @@
+from main import main
+#This demo file is for testing purposes only (Read the Quick Overview section in the README.md file for more information)
+main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
+
