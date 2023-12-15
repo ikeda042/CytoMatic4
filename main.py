d8ed22d825e31e09830d47ff7796a9f6c3fffbbf
ikeda042 <ikeda042main@gmail.com>
2023-12-15 22:23:31 +0900

    DA

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def main(
         # data_analysis(db_name=f"{file_name.split('.')[0]}.db", image_size=img_size,out_name = file_name.split(".")[0],single_layer_mode=single_layer_mode, dual_layer_mode=dual_layer_mode)
 
     elif mode == "data_analysis":
-        data_analysis_light(db_name=f"{file_name.split('.')[0]}.db", image_size=img_size,out_name = file_name.split(".")[0],single_layer_mode=single_layer_mode,dual_layer_mode=dual_layer_mode)
+        data_analysis(db_name=f"{file_name.split('.')[0]}.db", image_size=img_size,out_name = file_name.split(".")[0],single_layer_mode=single_layer_mode,dual_layer_mode=dual_layer_mode)
     # elif mode == "delete_all":
     #     delete_all(input_filename=file_name)
     elif mode == "delete_all":
