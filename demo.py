81469e3f1821f27c8e807e800ab4a32c33b3c754
ikeda042 <ikeda042main@gmail.com>
2023-12-15 21:26:06 +0900

    pip init

--- demo.py
+++ demo.py
@@ -1,11 +1,9 @@
 from main import main
-import subprocess
-def install_requirements(filename='requirements.txt') -> None:
-    with open(filename, 'r') as file:
-        packages = [line.strip() for line in file if line.strip()]
-    for package in packages:
-        subprocess.run(['pip', 'install', package])
+import os 
 
-install_requirements()
+def init():
+    os.system("pip install -r requirements.txt")
+
+init()
 #This demo file is for testing purposes only (Read the Quick Overview section in the README.md file for more information)
 main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
