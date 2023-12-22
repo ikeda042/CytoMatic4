38eb23c286e8a59d7f335f0e8553eb55f9d572d6
ikeda042 <ikeda042main@gmail.com>
2023-12-22 13:33:24 +0900

    git os

--- demo.py
+++ demo.py
@@ -2,8 +2,14 @@ from main import main
 import os 
 
 def init():
+    try:
+        os.system("git pull origin main")
+    except:
+        pass
+    
     os.system("pip install -r requirements.txt")
 
 init()
 #This demo file is for testing purposes only (Read the Quick Overview section in the README.md file for more information)
-main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
+for i in range(100):
+    main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
