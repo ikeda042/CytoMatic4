ccf04fefb542f62147a69565b2a9c5801a9f94b6
ikeda042 <ikeda042main@gmail.com>
2024-01-09 14:42:44 +0900

    demo

--- demo.py
+++ demo.py
@@ -10,5 +10,5 @@ def init():
 
 init()
 #This demo file is for testing purposes only (Read the Quick Overview section in the README.md file for more information)
-for i in range(100):
-    main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
+main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
+
