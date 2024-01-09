3d8fd979f2689729784137969a0c67bccdfad287
ikeda042 <ikeda042main@gmail.com>
2024-01-10 00:34:37 +0900

    format

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from main import main
-import os 
+import os
+
 
 def init():
     try:
@@ -8,7 +9,9 @@ def init():
         pass
     os.system("pip install -r requirements.txt")
 
-init()
-#This demo file is for testing purposes only (Read the Quick Overview section in the README.md file for more information)
-main("test_database.db", 85, 255, img_size = 200, mode="data_analysis", layer_mode="normal")
 
+init()
+# This demo file is for testing purposes only (Read the Quick Overview section in the README.md file for more information)
+main(
+    "test_database.db", 85, 255, img_size=200, mode="data_analysis", layer_mode="normal"
+)
