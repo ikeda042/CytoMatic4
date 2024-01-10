1f2934c8eae80a659f58e4806f27ecb9af8c9b09
ikeda042 <ikeda042main@gmail.com>
2024-01-10 12:14:50 +0900

    Fluo

--- DataAnalysis/fluo_localization_heatmap_analysis.py
+++ DataAnalysis/fluo_localization_heatmap_analysis.py
@@ -2,94 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
 
-# with open("temp_peak_analysis_tri/sk328tri0min_peak_points_ys.txt", "r") as f:
-#     ys_1 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
-#     ys_1_normalized = []
-#     for i in ys_1:
-#         i = np.array(i)
-#         i = (i - i.min()) / (i.max() - i.min())
-#         ys_1_normalized.append(i.tolist())
-
-# with open("temp_peak_analysis_tri/sk328tri30min_peak_points_ys.txt", "r") as f:
-#     ys_2 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
-#     ys_2_normalized = []
-#     for i in ys_2:
-#         i = np.array(i)
-#         i = (i - i.min()) / (i.max() - i.min())
-#         ys_2_normalized.append(i.tolist())
-
-# with open("temp_peak_analysis_tri/sk328tri60min_peak_points_ys.txt", "r") as f:
-#     ys_3 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
-#     ys_3_normalized = []
-#     for i in ys_3:
-#         i = np.array(i)
-#         i = (i - i.min()) / (i.max() - i.min())
-#         ys_3_normalized.append(i.tolist())
-
-# with open("temp_peak_analysis_tri/sk328tri90min_peak_points_ys.txt", "r") as f:
-#     ys_4 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
-#     ys_4_normalized = []
-#     for i in ys_4:
-#         i = np.array(i)
-#         i = (i - i.min()) / (i.max() - i.min())
-#         ys_4_normalized.append(i.tolist())
-
-# with open("temp_peak_analysis_tri/sk328tri120min_peak_points_ys.txt", "r") as f:
-#     ys_5 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
-#     ys_5_normalized = []
-#     for i in ys_5:
-#         i = np.array(i)
-#         i = (i - i.min()) / (i.max() - i.min())
-#         ys_5_normalized.append(i.tolist())
-
-
-with open("peakys2/sk25_bta_FITC_c_peak_points_ys.txt", "r") as f:
-    ys_1 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
+with open("sk320gen0min_peak_points_ys.txt", "r") as f:
+    ys_1 = [
+        [float(x.replace("\n", "")) for x in line.split(",")] for line in f.readlines()
+    ]
     ys_1_normalized = []
     for i in ys_1:
         i = np.array(i)
         i = (i - i.min()) / (i.max() - i.min())
         ys_1_normalized.append(i.tolist())
 
-with open("peakys2/sk25_bta_FITC_10_peak_points_ys.txt", "r") as f:
-    ys_2 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
+with open("sk320gen30min_peak_points_ys.txt", "r") as f:
+    ys_2 = [
+        [float(x.replace("\n", "")) for x in line.split(",")] for line in f.readlines()
+    ]
     ys_2_normalized = []
     for i in ys_2:
         i = np.array(i)
         i = (i - i.min()) / (i.max() - i.min())
         ys_2_normalized.append(i.tolist())
 
-with open("peakys2/sk25_bta_FITC_13_peak_points_ys.txt", "r") as f:
-    ys_3 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
+with open("sk320gen60min_peak_points_ys.txt", "r") as f:
+    ys_3 = [
+        [float(x.replace("\n", "")) for x in line.split(",")] for line in f.readlines()
+    ]
     ys_3_normalized = []
     for i in ys_3:
         i = np.array(i)
         i = (i - i.min()) / (i.max() - i.min())
         ys_3_normalized.append(i.tolist())
 
-with open("peakys2/sk25_bta_FITC_15_peak_points_ys.txt", "r") as f:
-    ys_4 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
+with open("sk320gen90min_peak_points_ys.txt", "r") as f:
+    ys_4 = [
+        [float(x.replace("\n", "")) for x in line.split(",")] for line in f.readlines()
+    ]
     ys_4_normalized = []
     for i in ys_4:
         i = np.array(i)
         i = (i - i.min()) / (i.max() - i.min())
         ys_4_normalized.append(i.tolist())
 
-with open("peakys2/sk25_bta_FITC_18_peak_points_ys.txt", "r") as f:
-    ys_5 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
+with open("sk320gen120min_peak_points_ys.txt", "r") as f:
+    ys_5 = [
+        [float(x.replace("\n", "")) for x in line.split(",")] for line in f.readlines()
+    ]
     ys_5_normalized = []
     for i in ys_5:
         i = np.array(i)
         i = (i - i.min()) / (i.max() - i.min())
         ys_5_normalized.append(i.tolist())
-
-with open("peakys2/sk25_bta_FITC_20_peak_points_ys.txt", "r") as f:
-    ys_6 = [[float(x.replace("\n","")) for x in line.split(",")] for line in f.readlines()]
-    ys_6_normalized = []
-    for i in ys_6:
-        i = np.array(i)
-        i = (i - i.min()) / (i.max() - i.min())
-        ys_6_normalized.append(i.tolist())
 
 
 print(ys_1_normalized[1])
@@ -97,12 +58,12 @@ print(ys_2_normalized[1])
 print(ys_3_normalized[1])
 print(ys_4_normalized[1])
 print(ys_5_normalized[1])
-print(ys_6_normalized[1])
+
 
 class HeadmapVector:
-    def __init__(self, heatmap_vector:np.ndarray, sample_num:int):
-        self.heatmap_vector:np.ndarray = heatmap_vector
-        self.sample_num:int = sample_num
+    def __init__(self, heatmap_vector: np.ndarray, sample_num: int):
+        self.heatmap_vector: np.ndarray = heatmap_vector
+        self.sample_num: int = sample_num
 
     def __gt__(self, other):
         # self_sum = np.sum([i for i in self.heatmap_vector if i >0.6])
@@ -113,43 +74,58 @@ class HeadmapVector:
         other_v = np.sum(other.heatmap_vector)
         return self_v < other_v
 
+
 # vectors = [HeadmapVector(i,1) for i in ys_1_normalized] + [HeadmapVector(i,2) for i in ys_2_normalized] + [HeadmapVector(i,3) for i in ys_3_normalized] + [HeadmapVector(i,4) for i in ys_4_normalized] + [HeadmapVector(i,5) for i in ys_5_normalized]
 
-vectors = sorted([HeadmapVector(i,1) for i in ys_1_normalized]) + sorted([HeadmapVector(i,2) for i in ys_2_normalized]) + sorted([HeadmapVector(i,3) for i in ys_3_normalized]) + sorted([HeadmapVector(i,4) for i in ys_4_normalized]) + sorted([HeadmapVector(i,5) for i in ys_5_normalized])+ sorted([HeadmapVector(i,6) for i in ys_6_normalized])
-
+vectors = (
+    sorted([HeadmapVector(i, 1) for i in ys_1_normalized])
+    + sorted([HeadmapVector(i, 2) for i in ys_2_normalized])
+    + sorted([HeadmapVector(i, 3) for i in ys_3_normalized])
+    + sorted([HeadmapVector(i, 4) for i in ys_4_normalized])
+    + sorted([HeadmapVector(i, 5) for i in ys_5_normalized])
+)
 
 
 # vectors = sorted([HeadmapVector(i,1) for i in ys_1]) + sorted([HeadmapVector(i,2) for i in ys_2]) + sorted([HeadmapVector(i,3) for i in ys_3]) + sorted([HeadmapVector(i,4) for i in ys_4]) + sorted([HeadmapVector(i,5) for i in ys_5])+ sorted([HeadmapVector(i,6) for i in ys_6])
 
 # vectors =  sorted([HeadmapVector(i,4) for i in ys_4_normalized]) + sorted([HeadmapVector(i,5) for i in ys_5_normalized])+ sorted([HeadmapVector(i,6) for i in ys_6_normalized])
-title = "sk328tri (sorted by sum of Normalized fluo. intensity)"
+title = "sk320 Gen (sorted by sum of Normalized fluo. intensity)"
 # vectors = sorted(vectors,reverse=True)
-concatenated_samples = np.column_stack([i.heatmap_vector for i in vectors])  # ベクトルを横に並べて結合
+concatenated_samples = np.column_stack(
+    [i.heatmap_vector for i in vectors]
+)  # ベクトルを横に並べて結合
 
 # 追加する横向きヒートマップのデータ（サンプルデータ)
-additional_row = np.array([i.sample_num/4 for i in vectors])[None, :]
+additional_row = np.array([i.sample_num / 4 for i in vectors])[None, :]
 
 plt.figure(figsize=(10, 6))
-gs = gridspec.GridSpec(2, 2, width_ratios=[30, 1], height_ratios=[1, 10], hspace=0.05, wspace=0.05)
+gs = gridspec.GridSpec(
+    2, 2, width_ratios=[30, 1], height_ratios=[1, 10], hspace=0.05, wspace=0.05
+)
 
 # 追加行のヒートマップ
 ax0 = plt.subplot(gs[0, 0])
-ax0.imshow(additional_row, aspect='auto', cmap='inferno', extent=[0, concatenated_samples.shape[1], 0, 1])
+ax0.imshow(
+    additional_row,
+    aspect="auto",
+    cmap="inferno",
+    extent=[0, concatenated_samples.shape[1], 0, 1],
+)
 ax0.set_xticks([])
 ax0.set_yticks([])
 
 # 元のヒートマップ
 ax1 = plt.subplot(gs[1, 0])
-im = ax1.imshow(concatenated_samples, aspect='auto', cmap='inferno')
-ax1.set_xlabel(f'Sample Number {title}')
-ax1.set_ylabel('Split index')
+im = ax1.imshow(concatenated_samples, aspect="auto", cmap="inferno")
+ax1.set_xlabel(f"Sample Number {title}")
+ax1.set_ylabel("Split index")
 
 # カラーバーを二つのグラフの外側に配置
 ax2 = plt.subplot(gs[:, 1])
 plt.colorbar(im, cax=ax2)
-ax2.set_ylabel('Normalized fluo. intensity', rotation=270, labelpad=15)
+ax2.set_ylabel("Normalized fluo. intensity", rotation=270, labelpad=15)
 
-plt.savefig("heatmap_FITC2.png")
+plt.savefig("heatmap_sk320Gen.png")
 
 # print(f"len_ys_1_normalized: {len(ys_1_normalized)}")
 # print(f"len_ys_2_normalized: {len(ys_1_normalized)+ len(ys_2_normalized)}")
@@ -162,36 +138,43 @@ plt.savefig("heatmap_FITC2.png")
 plt.close()
 
 
-fig_box = plt.figure(figsize=(6,6))
+# fig_box = plt.figure(figsize=(6, 6))
 
-data1 = [np.sum(i) for i in ys_1_normalized]
-data2 = [np.sum(i) for i in ys_2_normalized]
-data3 = [np.sum(i) for i in ys_3_normalized]
-data4 = [np.sum(i) for i in ys_4_normalized]
-data5 = [np.sum(i) for i in ys_5_normalized]
-data6 = [np.sum(i) for i in ys_6_normalized]
+# data1 = [np.sum(i) for i in ys_1_normalized]
+# data2 = [np.sum(i) for i in ys_2_normalized]
+# data3 = [np.sum(i) for i in ys_3_normalized]
+# data4 = [np.sum(i) for i in ys_4_normalized]
+# data5 = [np.sum(i) for i in ys_5_normalized]
 
-import numpy as np 
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=[8,8])
+# fig = plt.figure(figsize=[8, 8])
 
-# ボックスプロットを作成
-plt.boxplot([data1,data2,data3,data4,data5,data6],sym="")
+# # ボックスプロットを作成
+# plt.boxplot([data1, data2, data3, data4, data5], sym="")
 
-# データポイントをプロット（外れ値を含む）
-for i, data in enumerate([data1,data2,data3,data4,data5,data6], start=1):
-    x = np.random.normal(i, 0.04, size=len(data))  # データポイントのX座標を少しずらす
-    plt.plot(x, data, 'o', alpha=0.1)  
+# # データポイントをプロット（外れ値を含む）
+# for i, data in enumerate([data1, data2, data3, data4, data5], start=1):
+#     x = np.random.normal(i, 0.04, size=len(data))  # データポイントのX座標を少しずらす
+#     plt.plot(x, data, "o", alpha=0.1)
 
-# 各プロットにラベルを追加する
-plt.xticks([1, 2, 3, 4,5,6], [f'0% (n={len(data1)})', f'1.0%(n={len(data2)})', f'1.3% (n={len(data3)})', f'series4 (n={len(data4)})', f'series5 (n={len(data5)})', f'series6 (n={len(data6)})'])
+# # 各プロットにラベルを追加する
+# plt.xticks(
+#     [1, 2, 3, 4, 5],
+#     [
+#         f"0% (n={len(data1)})",
+#         f"1.0%(n={len(data2)})",
+#         f"1.3% (n={len(data3)})",
+#         f"series4 (n={len(data4)})",
+#         f" (n={len(data5)})",
+#     ],
+# )
 
-# グラフを表示
-plt.xlabel('Butanol ')
-plt.ylabel("Sum of Normalized fluo. intensity(-)")
-plt.grid(True)
+# # グラフを表示
+# plt.xlabel("Butanol ")
+# plt.ylabel("Sum of Normalized fluo. intensity(-)")
+# plt.grid(True)
 
-fig.savefig("result_sum.png",dpi = 500)
-plt.close()
-
+# fig.savefig("result_sum.png", dpi=500)
+# plt.close()
