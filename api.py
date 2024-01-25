4982a3a4c3dfcca203d7460294a7d5c01b9324cc
ikeda042 <ikeda042main@gmail.com>
2024-01-25 21:51:00 +0900

    async mat

--- api.py
+++ api.py
@@ -1,18 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
-from typing import List
+from typing import cast
 import aiofiles
+from app.DataAnalysis.load_mat_stackfiles import load_mat
 
-app = FastAPI(title="PhenoPixel4.0", version="0.0.0")
+app = FastAPI(title="PhenoPixel4.0")
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     content = await file.read()
-    async with aiofiles.open(content, "wb") as f:
+    filename = file.filename
+    async with aiofiles.open(filename, "wb") as f:
         await f.write(content)
-    return {"filename": file.filename, "content": content.decode()}
+    load_mat(cast(str, filename))
+    return {"filename": filename}
 
 @app.get("/get-overlay-image")
 async def get_overlay():
@@ -20,9 +23,11 @@ async def get_overlay():
     return FileResponse(file_path)
 
 @app.get("/get-heatmap")
-async def get_heatmap():
-    file_path = "Ph_com_mesh_signal_heatmap.png"  
+async def get_heatmap(file_name: str):
+    file_path = f"{file_name.replace('.mat','')}_heatmap.png"  
     return FileResponse(file_path)
 
+def main():
+    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000,reload=True)
+    main()
