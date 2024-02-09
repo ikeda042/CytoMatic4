1acf8360928cd3b60126ced879d6715d044e9047
ikeda042 <ikeda042main@gmail.com>
2024-02-09 13:49:56 +0900

    format

--- api.py
+++ api.py
@@ -10,9 +10,10 @@ from fastapi import HTTPException
 import os
 import shutil
 
-app = FastAPI(title="PhenoPixel4.0",docs_url="/phenopixel4.0")
+app = FastAPI(title="PhenoPixel4.0", docs_url="/phenopixel4.0")
 
-@app.post("/uploadfile/",tags=["ここでスタックファイル(.mat)をアップロード"])
+
+@app.post("/uploadfile/", tags=["ここでスタックファイル(.mat)をアップロード"])
 async def create_upload_file(file: UploadFile = File(...)):
     try:
         os.remove("mat_file.mat")
@@ -21,34 +22,42 @@ async def create_upload_file(file: UploadFile = File(...)):
         shutil.rmtree("Matlab")
     except:
         pass
-    print(file.filename,"uploaded+++++++++++++++++++++++++++++++++++++++++++++++")
+    print(file.filename, "uploaded+++++++++++++++++++++++++++++++++++++++++++++++")
     print(datetime.now())
     content = await file.read()
-    filename = cast(str,file.filename)
-    if not filename.endswith('.mat'):
-        raise HTTPException(status_code=400, detail="Invalid file extension. Only .mat files are allowed.")
+    filename = cast(str, file.filename)
+    if not filename.endswith(".mat"):
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid file extension. Only .mat files are allowed.",
+        )
     filename = "mat_file.mat"
     async with aiofiles.open(filename, "wb") as f:
         await f.write(content)
     load_mat(filename)
     # return FileResponse("mat_file_overlay.png")
-    return {"filename": file.filename,"status":"file uploaded"}
+    return {"filename": file.filename, "status": "file uploaded"}
 
-@app.get("/get-overlay-image",tags=["オーバーレイ画像を取得"])
+
+@app.get("/get-overlay-image", tags=["オーバーレイ画像を取得"])
 async def get_overlay():
     print("get_overlay+++++++++++++++++++++++++++++++++++++++++++++++")
     print(datetime.now())
-    file_path = "mat_file_overlay.png"  
+    file_path = "mat_file_overlay.png"
     return FileResponse(file_path)
 
-@app.get("/get-heatmap",tags=["ヒートマップを取得"])
+
+@app.get("/get-heatmap", tags=["ヒートマップを取得"])
 async def get_heatmap():
     print("get_heatmap+++++++++++++++++++++++++++++++++++++++++++++++")
     print(datetime.now())
-    file_path = f"mat_file_heatmap.png"  
+    file_path = f"mat_file_heatmap.png"
     return FileResponse(file_path)
+
 
 def main():
     uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+
+
 if __name__ == "__main__":
     main()
